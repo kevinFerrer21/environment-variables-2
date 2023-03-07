7d26b5eb69fd586038d1b4a6e09a536c2cb64eb3
Kevin <kebbo2110@gmail.com>
2023-03-07 15:22:27 +0100

    Initial commit

---
+++ src/main/java/co/develhope/environmentvariables2/controllers/MainController.java
@@ -0,0 +1,23 @@
+package co.develhope.environmentvariables2.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("main")
+public class MainController {
+
+    @Autowired
+    private Environment environment;
+
+
+    @GetMapping("/welcome")
+    public String welcomeMsgTest(){
+        String varTest = environment.getProperty("myCustomEnvs.welcomeMsg");
+        return varTest;
+    }
+
+}
